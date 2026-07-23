53e1ef477c215c46beaf982b7344c795d3f868cd
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-23 20:33:18 +0300

    Initialize jwt, cors and configurate my app

--- app.py
+++ app.py
@@ -2,12 +2,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+# Configure my app
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pawfind.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = "your-secret-key"
+
 
 # Initialize extensions
-app = Flask(__name__)
-db = SQLAlchemy()
-migrate = Migrate(db, app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+CORS(app)
 
+# Set up '/' route
 @app.route("/")
 def home():
     return {"message": "Pawfound API running"}
