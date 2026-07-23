3bce0e0d1995a5156260d6b47fa3861cc5220751
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-23 21:14:54 +0300

    Add a default role of adopter in the role column. Set maximum string size in password column to 100

--- models/user.py
+++ models/user.py
@@ -8,6 +8,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), nullable = False, unique = True)
     email = db.Column(db.String(80), nullable = False, unique = True)
-    role = db.Column(db.String(30), nullable = False)
-    password = db.Column(db.String(50), nullable = False)
+    role = db.Column(db.String(30), nullable = False, default = "adopter")
+    password = db.Column(db.String(100), nullable = False)
     created_at = db.Column(db.DateTime)
