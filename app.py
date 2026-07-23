188beac5057b64830c2317144dbaa6b0cb31abcb
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-23 20:19:13 +0300

    Import flask_sqlalchemy, flask_migrate and initialize those extensions as well as add comments

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+# imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# Initialize extensions
 app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate(db, app)
 
 @app.route("/")
 def home():
-    return {"message": "Pawfind API running"}
+    return {"message": "Pawfound API running"}
 
 
 if __name__ == "__main__":
