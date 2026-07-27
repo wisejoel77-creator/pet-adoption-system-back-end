99a6bf1bd9c012f1f1ed26b01939fdedb07bbcb2
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-27 23:26:23 +0300

    Completed user registration route

--- models/adoption_request.py
+++ models/adoption_request.py
@@ -1,5 +1,5 @@
 # Adoption_request.py file
-from app import db
+from extensions import db
 from datetime import datetime
 
 # Adoption request class
--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request 
 from models.user import User
+from extensions import bcrypt, db
 auth = Blueprint ("auth", __name__)
 
 @auth.route("/register", methods=["POST"])
@@ -13,7 +14,23 @@ def register():
 
     if username is None or username.strip() == "":
         return ("username must be filled in")
+    if email is None or ("@") not in email:
+     return("Invalid email format. Please put in the correct email")
+    if password is None or password.strip() == "":
+       return("Password field must not be empty")
 
     existing_user = User.query.filter_by(email=email).first()
-    
-    
+    if existing_user is not None:
+        return ("Error: A user with this email already exists. Use a different email"), 409
+
+    hashed_password = bcrypt.generate_password_hash(password)
+    password_hash = hashed_password.decode("utf-8")
+
+    new_user = User(username = username, email = email, role = role, password_hash = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return{
+       "message": "User created successfully",
+       "user_name": username
+    }
