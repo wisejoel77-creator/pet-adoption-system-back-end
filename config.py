dca6bfb79e09c1ea7e73528d6bb37e76210d5177
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-30 01:55:47 +0300

    Create a README.md file

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 # Config.py
 import os
+from datetime import timedelta
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///pawfound.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-this-in-production")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
