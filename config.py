421fddf962888c1872f90e7732caa8a62a899a12
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-30 22:28:51 +0300

    Use environment variable for database

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from datetime import timedelta
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///pawfound.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///pawfound.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-this-in-production")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
